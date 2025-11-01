0c79e8cd2324eacc73c6d45385a2607f619c68c5
Dev Patel <2000dev.p@gmail.com>
2025-11-01 14:30:22 -0400

    Save local changes before pushing

--- .history/server/controllers/auth.controller_20250605121501.js
+++
@@ -1,8 +0,0 @@
-import User from "../models/user.model.js";
-import jwt from "jsonwebtoken";
-import config from "./../../config/config.js";
-const signin = (req, res) => {};
-const signout = (req, res) => {};
-const requireSignin = "";
-const hasAuthorization = (req, res) => {};
-export default { signin, signout, requireSignin, hasAuthorization };
